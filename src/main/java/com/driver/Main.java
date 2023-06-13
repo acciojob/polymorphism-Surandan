ba4feb332442a724a7759a478d917f351dd5db1f
Surandan <110124420+Surandan@users.noreply.github.com>
2023-06-13 23:33:43 +0530

    AddedMySecSolution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,25 +1,21 @@
 package com.driver;
 class Product {
-      public int product(int x,int y) {
+    public int product(int x,int y) {
           return x*y;
       }
-    public double product(double x,double y) {
-        return x*y;
-    }
     public int product(int x,int y,int z) {
         return x*y*z;
     }
+    public double product(double x,double y) {
+        return x*y;
+    }
+
 }
 public class Main {
      public static void main(String args[]) {
          Product p = new Product();
-         double a = 3.0;
-         double b = 4.0;
-         int x = 3;
-         int y = 4;
-         int z = 5;
-         p.product(x,y);
+         p.product(3,4);
          p.product(3,4,5);
-         p.product(a,b);
+         p.product(3.45,4.56);
      }
 }
